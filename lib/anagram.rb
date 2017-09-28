62b7167b616cca70e6d1896d539d630df5ae5380
buckethead1986 <rspeckpfs@gmail.com>
2017-09-27 21:36:04 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :array
+
+  def initialize(array)
+    @array = array
+
+
+  end
+
+  def match(array)
+    array.select do |word|
+      # binding.pry
+      word.downcase.chars.sort.join == self.array.to_s.downcase.chars.sort.join
+    end
+  end
+
+end
